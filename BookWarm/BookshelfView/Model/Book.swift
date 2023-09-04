eb473683e3f1e57e9928925507c18ffc8bdb3e7b
JongHoon <qnm541@gmail.com>
2023-09-04 14:40:35 +0900

    [feature] add Book Table

--- BookWarm/BookshelfView/Model/Book.swift
+++ BookWarm/BookshelfView/Model/Book.swift
@@ -5,8 +5,28 @@
 //  Created by JongHoon on 2023/08/08.
 //
 
+import RealmSwift
+
 struct Book {
     let title: String
     let releaseDate: String
     let thumbnail: String
+}
+
+class BookTable: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var releaseDate: String
+    @Persisted var thumbnail: String
+    
+    convenience init(
+        title: String,
+        releaseDate: String,
+        thumbnail: String
+    ) {
+        self.init()
+        self.title = title
+        self.releaseDate = releaseDate
+        self.thumbnail = thumbnail
+    }
 }
